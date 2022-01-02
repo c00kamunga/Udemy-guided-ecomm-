1cbe1ec239ccdf5616df8c03cde3c0fc1a0dfe07
Barinder <tsothalanti2@yahoo.com>
2022-01-02 01:13:42 -0800

    added sign up form

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hi there')
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+  <div>
+    <form method="POST">
+    <input name="email" placeholder='email' />
+    <input name="password" placeholder='password' />
+    <input name="passwordConfirmation" placeholder='password confirmation' />
+    <button>Sign up</button>
+    </form>
+  </div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('listening');
+});
