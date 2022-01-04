c0e2960cfadd760789fed122bec28142002257e0
Barinder <tsothalanti2@yahoo.com>
2022-01-04 02:12:55 -0800

    added update method

--- repositories/users.js
+++ repositories/users.js
@@ -47,11 +47,23 @@ class UsersRepository {
     const filteredRecords = records.filter((record) => record.id !== id);
     await this.writeAll(filteredRecords);
   }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find((record) => record.id === id);
+
+    if (!record) {
+      throw new Error(`Record with ${id} not found`);
+    }
+
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
 
-  await repo.delete('385d4273');
+  await repo.update('def8ef82sefsefsef', { password: 'mypassword' });
 };
 test();
