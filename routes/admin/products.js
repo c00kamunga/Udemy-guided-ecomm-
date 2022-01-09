469e1aa420d083bd019f088d7e97547cb79464ba
Barinder <tsothalanti2@yahoo.com>
2022-01-09 02:46:16 -0800

    added styling to edit product form

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -42,9 +42,31 @@ router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
 
   if (!product) {
     return res.send('Product not found');
-
-    res.send(productsEditTemplate({ product }));
   }
+  res.send(productsEditTemplate({ product }));
 });
+
+router.post(
+  '/admin/products/:id/edit',
+  requireAuth,
+  upload.single('image'),
+  [requireTitle, requirePrice],
+  handleErrors(productsEditTemplate, async (req) => {
+    const product = await productsRepo.getOne(req.params.id);
+    return { product };
+  }),
+  async (req, res) => {
+    const changes = req.body;
+    if (req.file) {
+      changes.image = req.file.buffer.toString('base64');
+    }
+    try {
+      await productsRepo.update(req.params.id, changes);
+    } catch (err) {
+      return res.send('could not find item');
+    }
+    res.redirect('/admin/products');
+  }
+);
 
 module.exports = router;
